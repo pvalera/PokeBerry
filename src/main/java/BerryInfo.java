d7caf738c8f9c28cc278b9efca67587557dc8e12
pvalera <pvalera@hotmail.com>
2020-06-06 08:05:07 -0700

    Add files via upload

--- src/main/java/BerryInfo.java
+++ src/main/java/BerryInfo.java
@@ -1,9 +1,30 @@
 
 public class BerryInfo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public final String name;
+	public final int size;
+	public final int growthTime;
 
+	public BerryInfo(final String name, final int size, final int growthTime) {
+		this.name = name;;
+		this.size = size;
+		this.growthTime = growthTime;
 	}
 
+	public String toString() {
+		return name + " " + size + " " + growthTime;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public int getGrowthTime() {
+		return growthTime;
+	}
+	
 }
